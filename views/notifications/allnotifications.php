9a7643a5c3583693bcee7b804c3760d7240d5e00
Yesenia Diaz <yeseniadiazhernandez19@gmail.com>
2021-07-18 17:42:45 -0500

    18/Julio/2021 05:07::50

--- views/notifications/allnotifications.php
+++ views/notifications/allnotifications.php
@@ -1,110 +1,79 @@
+<?php
 
+use yii\helpers\Html;
+use yii\web\Controller;
+use app\models\Notifications;
+use yii\grid\GridView;
+?>
 <link href="<?= Yii::$app->homeUrl ?>css/notificationsStyle.css" rel="stylesheet" type="text/css"/>
-<!--
-<div class="Message" id="js-timer">
-    <div class="Message-icon">
-        <i class="fa fa-bell-o"></i>
-    </div>
-    <div class="Message-body">
-        <p>This is a simple, but friendly, notification.</p>
-        <p class="u-italic">It will disappear within a few seconds.</p>
-    </div>
-    <button class="Message-close js-messageClose"><i class="fa fa-times"></i></button>
-</div>
 
-<div class="Message Message--orange">
-    <div class="Message-icon">
-        <i class="fa fa-exclamation"></i>
-    </div>
-    <div class="Message-body">
-        <p>This is a simple notification with a brighter color...</p>
-        <p>If you bring you mouse here you can close it manually.</p>
-    </div>
-    <button class="Message-close js-messageClose"><i class="fa fa-times"></i></button>
-</div>
+<?php
+foreach ($alls as $all):
+    if ($all->read == "0") {
+        ?>
+        <div class="Message">
+            <div class="Message-icon">
+                <i class="fas fa-check"></i>
+            </div>
+            <div class="Message-body">
+                <h4><?= $all->title ?></h4>
+                <p> <?= $all->message ?></p>
+                <p class="pull-right"><i class="far fa-clock"></i> <?= $all->datatime ?></p>
+                <div class="btn-group" style="margin-top: 10px;">
+                    <?= Html::a('Leído', ['notifications/read', 'id' => $all->idnotifications], ['class' => 'Message-button', 'id' => 'js-showMe']) ?>
+                    <?= Html::a('Ver Oficio', ['office/evaluating', 'id' => $all->fkoffice], ['class' => 'Message-button js-messageClose']) ?>
+                </div>
 
+            </div>
+            <button class="Message-close js-messageClose"><i class="fa fa-times"></i></button>
+        </div>
+    <?php } else { ?>
+        <div class="Message Message--orange">
+            <div class="Message-icon">
+                <i class="fas fa-check-double"></i>
+            </div>
+            <div class="Message-body">
+                <h4><?= $all->title ?></h4>
+                <p> <?= $all->message ?></p>
+                <p class="pull-right" ><i class="far fa-clock"></i> <?= $all->datatime ?></p>
+                <div class="btn-group" style="margin-top: 10px;">
+                    <?= Html::a('Ver Oficio', ['office/evaluating', 'id' => $all->fkoffice], ['class' => 'Message-button js-messageClose']) ?>
+                </div>
 
-<div class="Message Message--green">
-    <div class="Message-icon">
-        <i class="fa fa-check"></i>
-    </div>
-    <div class="Message-body">
-        <p>This is a message telling you that everything is a-okay!</p>
-        <p>Good job, and good riddance.</p>
-    </div>
-    <button class="Message-close js-messageClose"><i class="fa fa-times"></i></button>
-</div>
-
-<div class="Message Message--red">
-    <div class="Message-icon">
-        <i class="fa fa-times"></i>
-    </div>
-    <div class="Message-body">
-        <p>This is a notification that something went wrong...</p>
-        <button class="Message-button" id="js-helpMe">Yikes, help me please!</button>
-        <button class="Message-button js-messageClose">Don't care.</button>
-    </div>
-    <button class="Message-close js-messageClose"><i class="fa fa-times"></i></button>
-</div>
--->
-<?php foreach ($alls as $all): 
-    if($all->read == "1"){       
-    ?>
-<div class="Message">
-    <div class="Message-icon">
-        <i class="fa fa-bell-o"></i>
-    </div>
-    <div class="Message-body">
-        <p>Do you know that you can assign status and relation to a company right in the visit list?</p>
-        <button class="Message-button" id="js-showMe">Show me how</button>
-        <button class="Message-button js-messageClose">Nah, not interested</button>
-    </div>
-    <button class="Message-close js-messageClose"><i class="fa fa-times"></i></button>
-</div>
-    <?php }else{ ?>
-<div class="Message Message--orange">
-    <div class="Message-icon">
-        <i class="fa fa-exclamation"></i>
-    </div>
-    <div class="Message-body">
-        <p>You haven't authorized your LinkedIn account. Would you like some help with that?</p>
-        <p class="u-italic">With your account connected we can show you what connections you have at a company that visited your site!</p>
-        <button class="Message-button" id="js-authMe">Authorize!</button>
-        <button class="Message-button js-messageClose">I'll just keep using carrier pigeons</button>
-    </div>
-    <button class="Message-close js-messageClose"><i class="fa fa-times"></i></button>
-</div>
-<?php } endforeach; ?>
+            </div>
+            <button class="Message-close js-messageClose"><i class="fa fa-times"></i></button>
+        </div>
+    <?php } endforeach; ?>
 
 <script>
     /*
-    function closeMessage(el) {
-        el.addClass('is-hidden');
-    }
-
-    $('.js-messageClose').on('click', function (e) {
-        closeMessage($(this).closest('.Message'));
-    });
-
-    $('#js-helpMe').on('click', function (e) {
-        alert('Help you we will, young padawan');
-        closeMessage($(this).closest('.Message'));
-    });
-
-    $('#js-authMe').on('click', function (e) {
-        alert('Okelidokeli, requesting data transfer.');
-        closeMessage($(this).closest('.Message'));
-    });
-
-    $('#js-showMe').on('click', function (e) {
-        alert("You're off to our help section. See you later!");
-        closeMessage($(this).closest('.Message'));
-    });
-
-    $(document).ready(function () {
-        setTimeout(function () {
-            closeMessage($('#js-timer'));
-        }, 5000);
-    });
-    */
+     function closeMessage(el) {
+     el.addClass('is-hidden');
+     }
+     
+     $('.js-messageClose').on('click', function (e) {
+     closeMessage($(this).closest('.Message'));
+     });
+     
+     $('#js-helpMe').on('click', function (e) {
+     alert('Help you we will, young padawan');
+     closeMessage($(this).closest('.Message'));
+     });
+     
+     $('#js-authMe').on('click', function (e) {
+     alert('Okelidokeli, requesting data transfer.');
+     closeMessage($(this).closest('.Message'));
+     });
+     
+     $('#js-showMe').on('click', function (e) {
+     alert("You're off to our help section. See you later!");
+     closeMessage($(this).closest('.Message'));
+     });
+     
+     $(document).ready(function () {
+     setTimeout(function () {
+     closeMessage($('#js-timer'));
+     }, 5000);
+     });
+     */
 </script>
